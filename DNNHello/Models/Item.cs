52de3b46d92c765371dd5e4b346f8054b7ae0d48
Robert Griffiths <robert.griffiths@stud.uni-corvinus.hu>
2026-04-28 14:54:17 +0200

    UT-MOD-01: Item globális láthatósági logika teszt

--- DNNHello/Models/Item.cs
+++ DNNHello/Models/Item.cs
@@ -19,5 +19,6 @@ namespace DNNHello.DNNHello.Models
         public DateTime LastModifiedOnDate { get; set; }
         public bool IsGlobal { get; set; }
         public bool IsUserApproved { get; set; }
+        public bool IsVisibleInGlobalGallery => IsGlobal && IsUserApproved;
     }
 }
